632ef42babf8362b8d965dbbb9373dd6d2067c8a
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-05 23:05:54 +0600

    Smallest element in List added

--- Minimum_in_list.py
+++ Minimum_in_list.py
@@ -7,3 +7,14 @@ for i in range(1, number + 1):
 print("The minimum element in list is: ", min(list))
 
 
+
+#Method 2
+list = [10, 20, 1, 45, 99]
+print("Smallest element is:", min(list))
+
+
+
+#Method 3
+List = [10, 20, 4, 45, 99]
+List.sort()
+print("Smallest element is:", *List[:1])
