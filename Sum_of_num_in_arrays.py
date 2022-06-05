330b556b2693996a7404c5b9c2a70a875aa748b8
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-05 12:52:35 +0600

    Sum of element of array added

--- Sum_of_num_in_arrays.py
+++ Sum_of_num_in_arrays.py
@@ -1,12 +1,19 @@
+#First Method
 def SumArray(array):
 	sum=0
 	for i in array:
 		sum = sum + i
 	return(sum)
-  
 array=[]
 array = [2, 5, 6, 8]
 n = len(array)
 x = SumArray(array)
 print ('Sum of the element in the array is ', x)
 
+
+
+#Second Method
+array = []
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+x = sum(array)
+print("The sum of element in array is ", x)
