f343f1b73946c55a1390fb389bfb254a5af10974
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-05 12:06:41 +0600

    Current date and time added

--- Current_time.py
+++ Current_time.py
@@ -1,4 +1,13 @@
+#Current Time
 from datetime import datetime
 time_now = datetime.now()
 currentTime = time_now.strftime("%H:%M:%S")
 print("Current Time in Bishkek is", currentTime)
+
+
+
+#Current Date and Time
+import datetime
+current_time = datetime.datetime.now()
+print("The current date and time in Bishkek is: ", end = " ")
+print(current_time)
