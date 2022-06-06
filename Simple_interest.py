6b27d728762cee6dcb3f5b67e4f5111f398ab9b7
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-06 12:14:24 +0600

    Simple Interest added

---
+++ Simple_interest.py
@@ -0,0 +1,13 @@
+
+def Simple_Interest(principal,time,rate):
+	print('The principal is', principal)
+	print('The time period is', time)
+	print('The rate of interest is',rate)
+	SimpleInterest = (principal * time * rate)/100
+	print('The Simple Interest is', SimpleInterest)
+	return SimpleInterest
+
+Simple_Interest(3000,2,4)
+
+
+
