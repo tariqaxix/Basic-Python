fe276f745a68689995fd9445ca06623d9bbc6ff5
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-06 11:35:30 +0600

    Even numbers in the list added

--- Even_Number_in_list.py
+++ Even_Number_in_list.py
@@ -1,7 +1,16 @@
+
+#Method 1
 Initial_number = int(input("Enter the strting number: "))
 Last_number = int(input("Enter the Last number: "))
 for i in range(Initial_number, Last_number + 1):
   if i % 2 == 0:
     print(i)
 
-    
+
+#Method 2
+for numbers in range(2, 27, 2):
+  print(numbers)
+
+
+
+
