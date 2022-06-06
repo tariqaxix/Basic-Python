934302f30e278e50b5d83e49ebb8340ffb68fd39
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-06 12:00:42 +0600

    Odd numbers in the list added

--- Odd_number_in_List.py
+++ Odd_number_in_List.py
@@ -1,6 +1,17 @@
 
+#Method 1
 Initial_number = int(input("Enter the initial number: "))
 Last_number = int(input("Enter the last number: "))
 for number in range(Initial_number, Last_number + 1):
   if number % 2 != 0:
     print(number)
+
+
+
+#Method 2
+List = [3, 36, 26, 89, 56, 92, 24, 72, 24]
+for number in List:
+  if number % 2 != 0:
+    print(number)
+  
+	
